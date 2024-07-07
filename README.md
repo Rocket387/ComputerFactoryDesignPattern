cdf0e85c4b682cff5709724586c5fcf0dc67d578
Rochelle Charles <roccharl@amazon.co.uk>
2024-07-07 16:11:00 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+
+
+Helpful links: https://www.digitalocean.com/community/tutorials/factory-design-pattern-in-java
---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,17 @@
+package org.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Computer pc = ComputerFactory.getComputer("pc", " 2GB", "500GB", "2.4GHz");
+        Computer server = ComputerFactory.getComputer("server","16 GB","1 TB","2.9 GHz");
+
+        System.out.println( "Factory PC Config: " + pc);
+        System.out.println( "Factory Server Config: " + server);
+    }
+}
