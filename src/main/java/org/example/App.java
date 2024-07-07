f465722c49dc6c001257e36151defee6af5587da
Rochelle Charles <roccharl@amazon.co.uk>
2024-07-07 16:19:09 +0100

    VM addition

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -10,8 +10,10 @@ public class App
     {
         Computer pc = ComputerFactory.getComputer("pc", " 2GB", "500GB", "2.4GHz");
         Computer server = ComputerFactory.getComputer("server","16 GB","1 TB","2.9 GHz");
+        Computer vm = ComputerFactory.getComputer("vm","16 GB","1 TB","3 GHz");
 
         System.out.println( "Factory PC Config: " + pc);
         System.out.println( "Factory Server Config: " + server);
+        System.out.println( "Factory VM Config: " + vm);
     }
 }
--- src/main/java/org/example/ComputerFactory.java
+++ src/main/java/org/example/ComputerFactory.java
@@ -4,7 +4,8 @@ public class ComputerFactory {
 
     public static Computer getComputer(String type, String ram, String hdd, String cpu){
         if("PC".equalsIgnoreCase(type)) return new PC(ram, hdd, cpu);
-        else if("Server".equalsIgnoreCase(type)) return new Server(ram, hdd, cpu);
+        if("Server".equalsIgnoreCase(type)) return new Server(ram, hdd, cpu);
+        else if("VM".equalsIgnoreCase(type)) return new VM(ram, hdd, cpu);
         return null;
     }
 }
